56b92a15cec9a61c27263cb922f90d63e8909311
DGNSHN <33982400+DgNShN@users.noreply.github.com>
2023-02-05 01:00:22 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("Benim")
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Giraffe)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
